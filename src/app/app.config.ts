0798466cd06449f58a79007916d9e2e8a41eb13e
Michael <mttenmick@email.com>
2026-08-10 12:30:49 +0300

    fix(config): provide router and HttpClient for API connectivity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,11 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from "@angular/core";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
-import { routes } from "./app.routes";
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZonelessChangeDetection(),
-    provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
-  ],
+    provideRouter(routes),
+    provideHttpClient()
+  ]
 };
