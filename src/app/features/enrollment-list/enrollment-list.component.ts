ff5f05af346d34436dce60603233e331edba135a
Michael <mttenmick@email.com>
2026-08-10 23:51:33 +0300

    feat(enrollment-list): refactor component to use Angular Material grid with sorting and pagination

--- src/app/features/enrollment-list/enrollment-list.component.ts
+++ src/app/features/enrollment-list/enrollment-list.component.ts
@@ -1,20 +1,39 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, viewChild, effect, inject } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
 import { EnrollmentStore } from '../../store/enrollment.store';
+import { Enrollment } from '../../models/enrollment.model';
 
 @Component({
   selector: 'tms-enrollment-list',
   standalone: true,
-  templateUrl: './enrollment-list.component.html'
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
+  templateUrl: './enrollment-list.component.html',
+  styleUrl: './enrollment-list.component.scss'
 })
-export class EnrollmentListComponent implements OnInit {
+export class EnrollmentListComponent {
   store = inject(EnrollmentStore);
 
-  ngOnInit() {
+  displayedColumns = ['studentName', 'courseName', 'status', 'actions'];
+  dataSource = new MatTableDataSource<Enrollment>();
+
+  readonly paginator = viewChild.required(MatPaginator);
+  readonly sort = viewChild.required(MatSort);
+
+  constructor() {
+    // Effect 1: Push store entities into Material’s data source whenever they change
+    effect(() => {
+      this.dataSource.data = this.store.entities();
+    });
+
+    // Effect 2: Wire paginator and sort controls reactively
+    effect(() => {
+      this.dataSource.paginator = this.paginator();
+      this.dataSource.sort = this.sort();
+    });
+
+    // Load enrollments immediately
     this.store.loadEnrollments();
   }
-
-  onApprove(id: string) {
-    this.store.approveEnrollment(id);
-  }
 }
-
