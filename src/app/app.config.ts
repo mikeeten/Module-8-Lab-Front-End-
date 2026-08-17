8a501a93ce135df31e544848cf8f77dc7168959a
Michael <mttenmick@email.com>
2026-08-17 18:53:43 +0300

    feat(app-config): configure HttpClient with credentials and XSRF support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+// import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-
+import { credentialsInterceptor } from './interceptors/credentials.interceptor';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([credentialsInterceptor]),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',   // matches .NET cookie
+        headerName: 'X-XSRF-TOKEN'  // matches .NET header
+      })
+    )
   ]
 };
