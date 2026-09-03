2cc473df16744ab70aaa7b42beb3170da54b9d79
Michael <mttenmick@email.com>
2026-09-03 21:48:05 +0300

    feat(ui): add conditional Admin action buttons to course-card template

--- src/app/ui/course-card/course-card.component.ts
+++ src/app/ui/course-card/course-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, input, output } from "@angular/core";
+import { Component, input, output, inject } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { Course } from "../../models/course.model";
+import { AuthService } from "../../../services/auth.service";
 
 @Component({
   selector: "tms-course-card",
@@ -10,6 +11,13 @@ import { Course } from "../../models/course.model";
   styleUrl: "./course-card.component.scss",
 })
 export class CourseCardComponent {
+  auth = inject(AuthService);
   course = input.required<Course>();
   enrollClicked = output<Course>();
+  deleteClicked = output<string | number>();
+
+  deleteCourse(id: string | number): void {
+    this.deleteClicked.emit(id);
+  }
 }
+
